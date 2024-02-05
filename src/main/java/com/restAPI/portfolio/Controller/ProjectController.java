ac7f96d6d48928f7af674dab5003f693eed06321
AzraeLandres <azraellandres@gmail.com>
2024-02-05 21:46:13 +0100

    routes

--- src/main/java/com/restAPI/portfolio/Controller/ProjectController.java
+++ src/main/java/com/restAPI/portfolio/Controller/ProjectController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/projects")
+@RequestMapping("/portfolio-azrael")
 public class ProjectController {
 
     @Autowired
@@ -37,7 +37,7 @@ public class ProjectController {
     }
     
     @CrossOrigin(origins = "*")
-    @GetMapping("/all")
+    @GetMapping("/")
     public List<ProjectDto> getProjects(){
         List<ProjectEntity> projects = projectService.getAllProjects();
         return projects.stream().map(ProjectMapper::mapToDto).collect(Collectors.toList());
