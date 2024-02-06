7be251f32c743aee35f2b6868b30a2acb4bae5f9
AzraeLandres <azraellandres@gmail.com>
2024-02-06 20:36:13 +0100

    route

--- src/main/java/com/restAPI/portfolio/Controller/AngularRoutingController.java
+++ src/main/java/com/restAPI/portfolio/Controller/AngularRoutingController.java
@@ -1,3 +1,4 @@
+
 package com.restAPI.portfolio.Controller;
 
 import org.springframework.stereotype.Controller;
@@ -6,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AngularRoutingController {
     
-    @RequestMapping(value = "/{path:[^\\.]*}")
+    @RequestMapping(value = "/**/{[path:[^\\.]*}")
     public String redirect() {
-        return "forward:/";
+        return "forward:/index.html";
     }
 }
