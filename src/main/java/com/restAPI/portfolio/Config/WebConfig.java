73bf630c6f317f2a781e27e245b32b5dbea0816e
AzraeLandres <azraellandres@gmail.com>
2024-02-06 21:33:55 +0100

    Add CORS configuration to WebConfig

--- src/main/java/com/restAPI/portfolio/Config/WebConfig.java
+++ src/main/java/com/restAPI/portfolio/Config/WebConfig.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
@@ -29,4 +30,12 @@ public class WebConfig implements WebMvcConfigurer{
             });
     }
     
+      @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://portfolio-azrael-81e2583d3a42.herokuapp.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
